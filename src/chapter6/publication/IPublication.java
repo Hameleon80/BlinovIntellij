8df36dd5b8c47d23bf7d3ae696a1d05ea4440bd9
АхтырскийЮра <goast80@ukr.net>
2021-08-10 08:39:45 +0400

    Start publication

---
+++ src/chapter6/publication/IPublication.java
@@ -0,0 +1,13 @@
+package chapter6.publication;
+
+/**
+ * Interface for entity publication.
+ *
+ * Ahtirskij Yuriy 10.08.2021
+ */
+public interface IPublication {
+    /**
+     * Show information about publication.
+     */
+    void showInfo();
+}
